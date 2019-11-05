eb2f25586d4fac1d3e45bed8e86a82f14a9da691
cexposit <cexposit@ull.edu.es>
2019-11-05 13:00:58 +0000

    minor change

--- jenkins/scripts/script-dependencies.sh
+++ jenkins/scripts/script-dependencies.sh
@@ -1,4 +1,9 @@
-git clone https://github.com/kaizten/kaizten-utils
+#git clone https://github.com/kaizten/kaizten-utils
+
+echo "script username is $USERNAME"
+echo "script password is $PASSWORD"
+
+git clone https://${USERNAME}:${PASSWORD}@github.com/kaizten/kaizten-utils
 pwd
 cp pom-utils.xml kaizten-utils/pom.xml
 cd kaizten-utils

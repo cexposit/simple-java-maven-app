4c5ccde4babb780abbc946182fd2e8e227059cb1
cexposit <cexposit@ull.edu.es>
2019-11-05 13:45:00 +0000

    minor change

--- jenkins/scripts/script-dependencies.sh
+++ jenkins/scripts/script-dependencies.sh
@@ -5,6 +5,11 @@ echo -n $USERNAME | wc -c
 echo "script password is $PASSWORD"
 echo -n $PASSWORD | wc -c
 
+export GIT_USER=$USERNAME
+export GIT_PASSWORD=$PASSWORD
+
+git config credential.helper '!f() { sleep 1; echo "username=${GIT_USER}"; echo "password=${GIT_PASSWORD}"; }; f'
+
 git clone https://$USERNAME:$PASSWORD@github.com/kaizten/kaizten-utils.git
 pwd
 cp pom-utils.xml kaizten-utils/pom.xml
